a089d502f6622f1c14825f7662b2a59a33d89448
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-09-12 15:05:21 -0700

    fix: make facade replacement variants valid JavaScript

--- scripts/v067-visual-fit-profiler.mjs
+++ scripts/v067-visual-fit-profiler.mjs
@@ -4,8 +4,8 @@ kinds=['fountain','hedge','pond','clock','stall','rage','soda','bomb','bakery','
 function rep(s,a,b=''){if(!s.includes(a))throw Error('anchor '+a.slice(0,50));return s.replace(a,b)}
 function noStreet(s){return rep(s,'drawTownStreetDecor();')}
 function lampOnly(s){return rep(s,"if((x/560|0)&1){X.fillStyle='#795640';X.fillRect(x+30,r[1]-22,38,6);X.fillRect(x+34,r[1]-14,30,5)}",'')}
-function simpleWindows(s){return rep(s,"for(let i=2;i--;){let a=x+14+i*(w-66);X.fillStyle='#f7ead8';X.fillRect(a-3,z-3,40,36);X.fillStyle='#9fe8ef';X.fillRect(a,z,34,30);X.fillStyle='#fff7';X.fillRect(a+16,z,2,30)}","X.fillStyle='#9fe8ef';for(let i=2;i--;)X.fillRect(x+14+i*(w-66),z,34,30)")}
-function solidAwning(s){return rep(s,"for(let i=5,s=(w-20)/5;i--;){X.fillStyle=i&1?'#f8ead8':c;X.fillRect(x+10+i*s,y+47,s+1,17)}","X.fillStyle=c;X.fillRect(x+10,y+47,w-20,17)")}
+function simpleWindows(s){return rep(s,"for(let i=2;i--;){let a=x+14+i*(w-66);X.fillStyle='#f7ead8';X.fillRect(a-3,z-3,40,36);X.fillStyle='#9fe8ef';X.fillRect(a,z,34,30);X.fillStyle='#fff7';X.fillRect(a+16,z,2,30)}","X.fillStyle='#9fe8ef';for(let i=2;i--;)X.fillRect(x+14+i*(w-66),z,34,30);")}
+function solidAwning(s){return rep(s,"for(let i=5,s=(w-20)/5;i--;){X.fillStyle=i&1?'#f8ead8':c;X.fillRect(x+10+i*s,y+47,s+1,17)}","X.fillStyle=c;X.fillRect(x+10,y+47,w-20,17);")}
 function oneBush(s){return rep(s,"townCircle(x+20,y+h-8,12,'#47794b');townCircle(x+w-20,y+h-8,12,'#527d50');","townCircle(x+20,y+h-8,12,'#47794b');")}
 function noAccent(s){return rep(s,"X.fillStyle=`hsl(${H} 65% 48%)`;k&1?townCircle(x+18,y+h-13,7):X.fillRect(x+10,y+h-18,18,10)",'')}
 function simplePeople(s){let a=s.indexOf('function drawPeople(){'),b=s.indexOf('function trail(',a);if(a<0||b<0)throw Error('people anchors');let f="function drawPeople(){for(let p of people){townCircle(p.x,p.y,9,`hsl(${p.h} 45% 70%)`);X.fillStyle='#263242';X.fillRect(p.x-2,p.y+8,4,25)}}\n";return s.slice(0,a)+f+s.slice(b)}
